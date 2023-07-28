0393342b591bc3d9dc3b04101bf694d558fa942e
Pranav Anbarasu <pranavanba@gmail.com>
2023-07-28 22:19:57 +0000

    Store df of all participants

--- get-parquet-info.R
+++ get-parquet-info.R
@@ -10,11 +10,11 @@ fitbitdevices <- arrow::open_dataset('./parquet/dataset_fitbitdevices/') %>% as_
 enrolled <- arrow::open_dataset('./parquet/dataset_enrolledparticipants/') %>% as_tibble()
 googlefit <- arrow::open_dataset('./parquet/dataset_googlefitsamples/') %>% as_tibble()
 hk_activitysummaries <- arrow::open_dataset('./parquet/dataset_healthkitv2activitysummaries/') %>% as_tibble() # No device vars
-hk_ecg <- arrow::open_dataset('./parquet/dataset_healthkitv2electrocardiogram/') %>% as_tibble() # Device_Name, Device_Model, Device_HardwareVersion, Source_Name (excluding Apple Watch and iPhone), StartDate
-hk_heartbeat <- arrow::open_dataset('./parquet/dataset_healthkitv2heartbeat/') %>% as_tibble() # Device_Name, Device_Model, Device_HardwareVersion, Source_Name (excluding Apple Watch and iPhone), StartDate
-hk_samples <- arrow::open_dataset('./parquet/dataset_healthkitv2samples/') %>% as_tibble() # Device_Name, Device_Model, Device_HardwareVersion, Source_Name (excluding Apple Watch and iPhone), StartDate
+hk_ecg <- arrow::open_dataset('./parquet/dataset_healthkitv2electrocardiogram/') %>% as_tibble() # Device_Name, Device_Model, Device_HardwareVersion, Source_Identifier (replace_na Apple Watch and iPhone), StartDate
+hk_heartbeat <- arrow::open_dataset('./parquet/dataset_healthkitv2heartbeat/') %>% as_tibble() # Device_Name, Device_Model, Device_HardwareVersion, Source_Name (replace_na Apple Watch and iPhone), StartDate
+hk_samples <- arrow::open_dataset('./parquet/dataset_healthkitv2samples/') %>% as_tibble() # Device_Name, Device_Model, Device_HardwareVersion, Source_Name (replace_na Apple Watch and iPhone), StartDate
 hk_stats <- arrow::open_dataset('./parquet/dataset_healthkitv2statistics/') %>% as_tibble() # No device vars
-hk_workouts <- arrow::open_dataset('./parquet/dataset_healthkitv2workouts/') %>% as_tibble() # Device_Name, Device_Model, Device_HardwareVersion, Source_Name (excluding Apple Watch and iPhone), StartDate
+hk_workouts <- arrow::open_dataset('./parquet/dataset_healthkitv2workouts/') %>% as_tibble() # Device_Name, Device_Model, Device_HardwareVersion, Source_Name (replace_na Apple Watch and iPhone), StartDate
 symptomlog <- arrow::open_dataset('./parquet/dataset_symptomlog/') %>% as_tibble()
 
 # Count number of records by counting number of rows across all parquet datasets
@@ -26,3 +26,30 @@ for (dir in list.dirs('./parquet/', recursive = F)) {
   pq_n_records <- bind_rows(pq_n_records, current)
 }
 
+all_participants <- tibble(dataset = character(), ParticipantIdentifier = character())
+all_participants <- 
+  bind_rows(all_participants, 
+            (fitbitdevices %>% 
+               select(ParticipantIdentifier) %>% 
+               mutate(dataset = 'fitbitdevices'))) %>% 
+  bind_rows((googlefit %>% 
+               select(ParticipantIdentifier) %>% 
+               mutate(dataset = 'googlefit'))) %>% 
+  bind_rows((hk_activitysummaries %>% 
+               select(ParticipantIdentifier) %>% 
+               mutate(dataset = 'healthkit'))) %>% 
+  bind_rows((hk_ecg %>% 
+               select(ParticipantIdentifier) %>% 
+               mutate(dataset = 'healthkit'))) %>% 
+  bind_rows((hk_heartbeat %>% 
+               select(ParticipantIdentifier) %>% 
+               mutate(dataset = 'healthkit'))) %>% 
+  bind_rows((hk_samples %>% 
+               select(ParticipantIdentifier) %>% 
+               mutate(dataset = 'healthkit'))) %>% 
+  bind_rows((hk_stats %>% 
+               select(ParticipantIdentifier) %>% 
+               mutate(dataset = 'healthkit'))) %>% 
+  bind_rows((hk_workouts %>% 
+               select(ParticipantIdentifier) %>% 
+               mutate(dataset = 'healthkit')))
