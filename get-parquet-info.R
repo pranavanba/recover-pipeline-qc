b01f14d00509f5349f78039c4eb823ed55ffe0dd
Pranav Anbarasu <pranavanba@gmail.com>
2023-07-28 20:47:13 +0000

    Get number of records across all parquet datasets

--- get-parquet-info.R
+++ get-parquet-info.R
@@ -3,6 +3,19 @@
 # setwd('~/recover-pipeline-qc/')
 
 library(arrow)
+library(tidyr)
+library(dplyr)
 
 fitbitdevices <- arrow::open_dataset('./parquet/dataset_fitbitdevices/') %>% as.data.frame()
 enrolled <- arrow::open_dataset('./parquet/dataset_enrolledparticipants/') %>% as.data.frame()
+googlefit <- arrow::open_dataset('./parquet/dataset_googlefitsamples/') %>% as.data.frame()
+
+# Count number of records by counting number of rows across all parquet datasets
+pq_n_records <- tibble()
+for (dir in list.dirs('./parquet/', recursive = F)) {
+  rows <- open_dataset(dir) %>% nrow()
+  name <- basename(dir)
+  current <- tibble(dataset = name, n_records = rows)
+  pq_n_records <- bind_rows(pq_n_records, current)
+}
+
