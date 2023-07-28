c87e182ea185d1b3e82bc80401a7dbb80eed1aa6
Pranav Anbarasu <pranavanba@gmail.com>
2023-07-28 21:45:55 +0000

    Identify device and data metadata columns in healthkit parquet datasets

--- get-parquet-info.R
+++ get-parquet-info.R
@@ -9,12 +9,12 @@ library(dplyr)
 fitbitdevices <- arrow::open_dataset('./parquet/dataset_fitbitdevices/') %>% as_tibble()
 enrolled <- arrow::open_dataset('./parquet/dataset_enrolledparticipants/') %>% as_tibble()
 googlefit <- arrow::open_dataset('./parquet/dataset_googlefitsamples/') %>% as_tibble()
-hk_activitysummaries <- arrow::open_dataset('./parquet/dataset_healthkitv2activitysummaries/') %>% as_tibble()
-hk_ecg <- arrow::open_dataset('./parquet/dataset_healthkitv2electrocardiogram/') %>% as_tibble()
-hk_heartbeat <- arrow::open_dataset('./parquet/dataset_healthkitv2heartbeat/') %>% as_tibble()
-hk_samples <- arrow::open_dataset('./parquet/dataset_healthkitv2samples/') %>% as_tibble()
-hk_stats <- arrow::open_dataset('./parquet/dataset_healthkitv2statistics/') %>% as_tibble()
-hk_workouts <- arrow::open_dataset('./parquet/dataset_healthkitv2workouts/') %>% as_tibble()
+hk_activitysummaries <- arrow::open_dataset('./parquet/dataset_healthkitv2activitysummaries/') %>% as_tibble() # No device vars
+hk_ecg <- arrow::open_dataset('./parquet/dataset_healthkitv2electrocardiogram/') %>% as_tibble() # Device_Name, Device_Model, Device_HardwareVersion, Source_Name (excluding Apple Watch and iPhone), StartDate
+hk_heartbeat <- arrow::open_dataset('./parquet/dataset_healthkitv2heartbeat/') %>% as_tibble() # Device_Name, Device_Model, Device_HardwareVersion, Source_Name (excluding Apple Watch and iPhone), StartDate
+hk_samples <- arrow::open_dataset('./parquet/dataset_healthkitv2samples/') %>% as_tibble() # Device_Name, Device_Model, Device_HardwareVersion, Source_Name (excluding Apple Watch and iPhone), StartDate
+hk_stats <- arrow::open_dataset('./parquet/dataset_healthkitv2statistics/') %>% as_tibble() # No device vars
+hk_workouts <- arrow::open_dataset('./parquet/dataset_healthkitv2workouts/') %>% as_tibble() # Device_Name, Device_Model, Device_HardwareVersion, Source_Name (excluding Apple Watch and iPhone), StartDate
 symptomlog <- arrow::open_dataset('./parquet/dataset_symptomlog/') %>% as_tibble()
 
 # Count number of records by counting number of rows across all parquet datasets
