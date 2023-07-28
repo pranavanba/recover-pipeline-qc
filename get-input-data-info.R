d2d3d009612e0311c64a57d0f5a483c313a65ca8
Pranav Anbarasu <pranavanba@gmail.com>
2023-07-28 20:03:12 +0000

    Rename to get-input-data-info

--- get-input-data-info.R
+++ get-input-data-info.R
@@ -121,53 +121,43 @@ devices_adults <-
   tibble::enframe() %>% 
   tidyr::unnest(cols = c(value))
 
-pids_ped <- apply_function_to_subdirectories(pediatric_folder_path, get_unique_participant_ids)
-pids_enroll_ped <- 
-  apply_function_to_subdirectories(pediatric_folder_path, get_unique_participant_ids_enroll_date) %>% 
-  bind_rows(.id = "list_name")
-
-nrecs_ped <- apply_function_to_subdirectories(pediatric_folder_path, get_num_rows_per_file)
-nrecs_date_ped <- 
-  apply_function_to_subdirectories(pediatric_folder_path, get_num_rows_per_file) %>% 
-  bind_rows(.id = "list_name")
-
-devices_ped <- 
-  apply_function_to_subdirectories(pediatric_folder_path, get_unique_fitbit_devices) %>% 
-  tibble::enframe() %>% 
-  tidyr::unnest(cols = c(value))
+# pids_ped <- apply_function_to_subdirectories(pediatric_folder_path, get_unique_participant_ids)
+# pids_enroll_ped <- 
+#   apply_function_to_subdirectories(pediatric_folder_path, get_unique_participant_ids_enroll_date) %>% 
+#   bind_rows(.id = "list_name")
+# 
+# nrecs_ped <- apply_function_to_subdirectories(pediatric_folder_path, get_num_rows_per_file)
+# nrecs_date_ped <- 
+#   apply_function_to_subdirectories(pediatric_folder_path, get_num_rows_per_file) %>% 
+#   bind_rows(.id = "list_name")
+# 
+# devices_ped <- 
+#   apply_function_to_subdirectories(pediatric_folder_path, get_unique_fitbit_devices) %>% 
+#   tibble::enframe() %>% 
+#   tidyr::unnest(cols = c(value))
 
 #### Total counts ###
 total_unique_pids_adult <- 
   pids_enroll_adults$ParticipantIdentifier %>% 
   n_distinct()
-total_unique_pids_ped <- 
-  pids_enroll_ped$ParticipantIdentifier %>% 
-  n_distinct()
+# total_unique_pids_ped <- 
+#   pids_enroll_ped$ParticipantIdentifier %>% 
+#   n_distinct()
 
 most_recent_enroll_date_adult <- 
   pids_enroll_adults$EnrollmentDate %>% 
   date() %>% 
   max()
-most_recent_enroll_date_ped <- 
-  pids_enroll_ped$EnrollmentDate %>% 
-  date() %>% 
-  max()
+# most_recent_enroll_date_ped <- 
+#   pids_enroll_ped$EnrollmentDate %>% 
+#   date() %>% 
+#   max()
 
 ### Plots ###
 
 # Particpants
 pid_cumulative_counts_adults <- 
   pids_enroll_adults %>% 
-  group_by(ParticipantIdentifier, EnrollmentDate) %>% 
-  distinct(ParticipantIdentifier, EnrollmentDate, .keep_all = T) %>% 
-  ungroup() %>% 
-  mutate(EnrollmentDate=lubridate::date(EnrollmentDate)) %>% 
-  group_by(EnrollmentDate) %>%
-  summarise(count = n_distinct(ParticipantIdentifier)) %>%
-  mutate(running_total = cumsum(count))
-
-pid_cumulative_counts_ped <- 
-  pids_enroll_ped %>% 
   group_by(ParticipantIdentifier, EnrollmentDate) %>% 
   distinct(ParticipantIdentifier, EnrollmentDate, .keep_all = T) %>% 
   ungroup() %>% 
@@ -185,14 +175,24 @@ a <- ggline(data = pid_cumulative_counts_adults,
   grids()
 ggpar(a, caption = glue::glue('n = {total_unique_pids_adult} unique participants as of {most_recent_enroll_date_adult}'))
 
-p <- ggline(data = pid_cumulative_counts_ped, 
-            x = 'EnrollmentDate', 
-            y = 'running_total',
-            xlab = "Date",
-            ylab = "Cumulative Count of Unique Participant IDs",
-            title = "Enrollment Over Time (Pediatric)") + 
-  grids()
-ggpar(p, caption = glue::glue('n = {total_unique_pids_ped} unique participants as of {most_recent_enroll_date_ped}'))
+# pid_cumulative_counts_ped <- 
+#   pids_enroll_ped %>% 
+#   group_by(ParticipantIdentifier, EnrollmentDate) %>% 
+#   distinct(ParticipantIdentifier, EnrollmentDate, .keep_all = T) %>% 
+#   ungroup() %>% 
+#   mutate(EnrollmentDate=lubridate::date(EnrollmentDate)) %>% 
+#   group_by(EnrollmentDate) %>%
+#   summarise(count = n_distinct(ParticipantIdentifier)) %>%
+#   mutate(running_total = cumsum(count))
+# 
+# p <- ggline(data = pid_cumulative_counts_ped, 
+#             x = 'EnrollmentDate', 
+#             y = 'running_total',
+#             xlab = "Date",
+#             ylab = "Cumulative Count of Unique Participant IDs",
+#             title = "Enrollment Over Time (Pediatric)") + 
+#   grids()
+# ggpar(p, caption = glue::glue('n = {total_unique_pids_ped} unique participants as of {most_recent_enroll_date_ped}'))
 
 # Number of records
 # tmp <- 
@@ -225,28 +225,28 @@ a2 <- ggpie(pie_adult, 'dataset_sum', label = c('','','',''),
 ggpar(a2, caption = glue::glue('n = {sum(pie_adult$dataset_sum)} records as of {max(nrecs_date_adults$ExportStartDate)}'), 
       title = "Proportion of Records from Different Datasets (Adults)")
 
-pie_ped <- 
-  nrecs_date_ped %>% 
-  group_by(file) %>% 
-  summarise(sum=sum(num_records)) %>% 
-  ungroup() %>% 
-  mutate(dataset = ifelse(grepl('HealthKit', file), 
-                          'HealthKit', 
-                          ifelse(grepl('Intraday', file), 
-                                 'FitbitIntradayCombined', 
-                                 ifelse(grepl('Enrolled', file),
-                                        'EnrolledParticipants',
-                                        'Fitbit_Other')))) %>% 
-  group_by(dataset) %>% 
-  summarise(dataset_sum = sum(sum)) %>% 
-  ungroup() %>% 
-  filter(dataset_sum>0)
-
-labs_ped <- paste0(pie_ped$dataset, " (", (prop.table(pie_ped$dataset_sum)*100) %>% round(2), "%)")
-p2 <- ggpie(pie_ped, 'dataset_sum', label = labs_ped, 
-            fill = 'dataset')
-ggpar(p2, caption = glue::glue('n = {sum(pie_ped$dataset_sum)} records as of {max(nrecs_date_ped$ExportStartDate)}'), 
-      title = "Proportion of Records from Different Datasets (Pediatric)")
+# pie_ped <- 
+#   nrecs_date_ped %>% 
+#   group_by(file) %>% 
+#   summarise(sum=sum(num_records)) %>% 
+#   ungroup() %>% 
+#   mutate(dataset = ifelse(grepl('HealthKit', file), 
+#                           'HealthKit', 
+#                           ifelse(grepl('Intraday', file), 
+#                                  'FitbitIntradayCombined', 
+#                                  ifelse(grepl('Enrolled', file),
+#                                         'EnrolledParticipants',
+#                                         'Fitbit_Other')))) %>% 
+#   group_by(dataset) %>% 
+#   summarise(dataset_sum = sum(sum)) %>% 
+#   ungroup() %>% 
+#   filter(dataset_sum>0)
+# 
+# labs_ped <- paste0(pie_ped$dataset, " (", (prop.table(pie_ped$dataset_sum)*100) %>% round(2), "%)")
+# p2 <- ggpie(pie_ped, 'dataset_sum', label = labs_ped, 
+#             fill = 'dataset')
+# ggpar(p2, caption = glue::glue('n = {sum(pie_ped$dataset_sum)} records as of {max(nrecs_date_ped$ExportStartDate)}'), 
+#       title = "Proportion of Records from Different Datasets (Pediatric)")
 
 nrecs_cumsum_adult <- 
   nrecs_date_adults %>% 
@@ -260,17 +260,17 @@ a3 <- ggline(nrecs_cumsum_adult, 'ExportStartDate', 'cumsum',
   grids()
 ggpar(a3, caption = glue::glue('n = {max(nrecs_cumsum_adult$cumsum)} records as of {max(nrecs_cumsum_adult$ExportStartDate)}'))
 
-nrecs_cumsum_ped <- 
-  nrecs_date_ped %>% 
-  group_by(ExportStartDate) %>% 
-  reframe(sum=sum(num_records)) %>% 
-  reframe(ExportStartDate, cumsum=cumsum(sum))
-
-p3 <- ggline(nrecs_cumsum_ped, 'ExportStartDate', 'cumsum',
-             xlab = 'Export Date', ylab = '# of Records',
-             title = '# of Records Across All Exports Over Time (Pediatric)') + 
-  grids()
-ggpar(p3, caption = glue::glue('n = {max(nrecs_cumsum_ped$cumsum)} records as of {max(nrecs_cumsum_ped$ExportStartDate)}'))
+# nrecs_cumsum_ped <- 
+#   nrecs_date_ped %>% 
+#   group_by(ExportStartDate) %>% 
+#   reframe(sum=sum(num_records)) %>% 
+#   reframe(ExportStartDate, cumsum=cumsum(sum))
+# 
+# p3 <- ggline(nrecs_cumsum_ped, 'ExportStartDate', 'cumsum',
+#              xlab = 'Export Date', ylab = '# of Records',
+#              title = '# of Records Across All Exports Over Time (Pediatric)') + 
+#   grids()
+# ggpar(p3, caption = glue::glue('n = {max(nrecs_cumsum_ped$cumsum)} records as of {max(nrecs_cumsum_ped$ExportStartDate)}'))
 
 # file_cumsum_nrecs <- 
 #   nrecs_date_adults %>% 
@@ -279,10 +279,5 @@ ggpar(p3, caption = glue::glue('n = {max(nrecs_cumsum_ped$cumsum)} records as of
 #   reframe(file, ExportStartDate, cumsum=cumsum(sum))
 
 # Devices
-devices_summary_adult <- 
-  devices_adults %>% 
-  select(ParticipantIdentifier, Device) %>% 
-  group_by(Device) %>% 
-  summarise(n_participants = n_distinct(ParticipantIdentifier))
 
-devices_ped$value %>% unique() %>% sort()
+# devices_ped$value %>% unique() %>% sort()
