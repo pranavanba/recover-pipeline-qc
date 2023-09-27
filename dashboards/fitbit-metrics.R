886336ddf553ae737a01c9e91ec63b72b2169564
Pranav Anbarasu <pranavanba@gmail.com>
2023-09-27 21:59:54 +0000

    Rename files and move to unified location

--- dashboards/fitbit-metrics.R
+++ dashboards/fitbit-metrics.R
@@ -8,29 +8,29 @@ library(lubridate)
 
 synLogin()
 
-token <- synapser::synGetStsStorageToken(
-  entity = 'syn51406699',
-  permission = "read_only",
-  output_format = "json")
-
-PARQUET_BUCKET <- 'recover-processed-data'
-PARQUET_BUCKET_BASE_KEY <- 'main/parquet/'
-
-if (PARQUET_BUCKET==token$bucket && PARQUET_BUCKET_BASE_KEY==token$baseKey) {
-  base_s3_uri <- paste0('s3://', token$bucket, '/', token$baseKey)
-} else {
-  base_s3_uri <- paste0('s3://', PARQUET_BUCKET, '/', PARQUET_BUCKET_BASE_KEY)
-}
-
-# configure the environment with AWS token
-Sys.setenv('AWS_ACCESS_KEY_ID'=token$accessKeyId,
-           'AWS_SECRET_ACCESS_KEY'=token$secretAccessKey,
-           'AWS_SESSION_TOKEN'=token$sessionToken)
-
-AWS_PARQUET_DOWNLOAD_LOCATION <- '~/recover-pipeline-qc/parquet/'
-unlink(AWS_PARQUET_DOWNLOAD_LOCATION, recursive = T, force = T)
-sync_cmd <- glue::glue('aws s3 sync {base_s3_uri} {AWS_PARQUET_DOWNLOAD_LOCATION} --exclude "*owner.txt*" --exclude "*archive*"')
-system(sync_cmd)
+# token <- synapser::synGetStsStorageToken(
+#   entity = 'syn51406699',
+#   permission = "read_only",
+#   output_format = "json")
+# 
+# PARQUET_BUCKET <- 'recover-processed-data'
+# PARQUET_BUCKET_BASE_KEY <- 'main/parquet/'
+# 
+# if (PARQUET_BUCKET==token$bucket && PARQUET_BUCKET_BASE_KEY==token$baseKey) {
+#   base_s3_uri <- paste0('s3://', token$bucket, '/', token$baseKey)
+# } else {
+#   base_s3_uri <- paste0('s3://', PARQUET_BUCKET, '/', PARQUET_BUCKET_BASE_KEY)
+# }
+# 
+# # configure the environment with AWS token
+# Sys.setenv('AWS_ACCESS_KEY_ID'=token$accessKeyId,
+#            'AWS_SECRET_ACCESS_KEY'=token$secretAccessKey,
+#            'AWS_SESSION_TOKEN'=token$sessionToken)
+# 
+# AWS_PARQUET_DOWNLOAD_LOCATION <- '~/recover-pipeline-qc/parquet/'
+# unlink(AWS_PARQUET_DOWNLOAD_LOCATION, recursive = T, force = T)
+# sync_cmd <- glue::glue('aws s3 sync {base_s3_uri} {AWS_PARQUET_DOWNLOAD_LOCATION} --exclude "*owner.txt*" --exclude "*archive*"')
+# system(sync_cmd)
 
 fitbitdailydata <- 
   open_dataset('./parquet/dataset_fitbitdailydata/') %>% 
@@ -85,14 +85,18 @@ num_participants_with_records_nonzero <- function(df, measure) {
     length()
 }
 
-days_present_nonzero_per_participant <- function(df, measure) {
-  fitbitdailydata %>% 
+days_present_nonzero_per_participant <- function(df, measure, enrollment_df) {
+  df %>% 
     select(ParticipantIdentifier, Date, measure) %>% 
     mutate(Date = as_date(Date)) %>% 
     drop_na() %>% 
     filter(.[[measure]]!=0) %>% 
     group_by(ParticipantIdentifier) %>% 
     distinct(Date, .keep_all = T) %>% 
+    # merge(y = (enrollment_df %>% select(ParticipantIdentifier, EnrollmentDate))) %>% 
+    # mutate(EnrollmentDate=as_date(EnrollmentDate)) %>% 
+    # filter(Date>=EnrollmentDate) %>% 
+    # group_by(ParticipantIdentifier) %>% 
     summarise(days_present_nonzero = n()) %>% 
     ungroup()
 }
